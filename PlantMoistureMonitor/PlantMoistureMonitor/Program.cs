cb4522c50bca645d3683597c5d6cde4769acaea1
Valentin Vadasz <vadaszvalentin@gmail.com>
2022-07-17 16:40:14 +0200

    Initial commit

---
+++ PlantMoistureMonitor/PlantMoistureMonitor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PlantMoistureMonitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
