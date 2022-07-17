ac7b03dee09c8902e7e2b6190c719fb4f1af03b6
Tóth Balázs <tothbalou@gmail.com>
2022-07-17 17:14:05 +0200

    Welcome user

--- PlantMoistureMonitor/PlantMoistureMonitor/Program.cs
+++ PlantMoistureMonitor/PlantMoistureMonitor/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string? name;
+            name = GetName();
+            WriteName(name);
+        }
+
+        static string? GetName()
+        {
+            Console.WriteLine("Add meg a neved:");
+
+            string? name;
+            name = Console.ReadLine();
+
+            return name;
+        }
+
+        static void WriteName(string? userName)
+        {
+            Console.WriteLine("Üdvözöllek, {0}", userName);
         }
     }
 }
